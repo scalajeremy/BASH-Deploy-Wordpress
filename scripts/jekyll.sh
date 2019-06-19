8e0256522dfe8964bb07c22ea7185f49eac7a17d
Jeremy Scala <scalajeremy@gmail.com>
2019-06-19 12:25:32 +0200

    ask os and shell + colors

--- scripts/jekyll.sh
+++ scripts/jekyll.sh
@@ -2,6 +2,14 @@
 # Installing environnement for Jekyll. 
 # V1
 
+# Styling
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+
 # Functions
 
 function connectionIsUp () {
@@ -12,22 +20,32 @@ function connectionIsUp () {
   fi
 }
 
-function installRuby () {
-    sudo apt-get install ruby-full build-essential zlib1g-dev
-}
-
-function whatShell () {
-  echo "Are you using BASH or ZSH?"
-select shell in "Bash" "ZSH"; do
-    case $shell in
-        Bash ) shellSelected; exit;;
-        ZSH )  shellSelected; exit;;
+function whatOs () {
+  clear
+  echo "Can you tell me which OS you're working on?"
+  echo "Linux or MacOSX?"
+select os in "Linux" "MacOSX"; do
+    case $os in
+        Linux ) break;;
+        MacOSX )  break;;
     esac
 done
 }
 
-function shellSelected () {
-  echo "Coucou" + $shell
+function whatShell () {
+  clear
+  echo "In order to link to your gems properly, I need to know which shell you're using"
+  echo "Are you using BASH or ZSH?"
+select shell in "Bash" "ZSH"; do
+    case $shell in
+        Bash ) break;;
+        ZSH )  break;;
+    esac
+done
+}
+
+function installRuby () {
+    sudo apt-get install ruby-full build-essential zlib1g-dev
 }
 
 ###################
@@ -38,6 +56,18 @@ connectionIsUp
 if [[ $? != 0 ]]; then
     exit 1
 fi
+whatOs
 whatShell
+clear
+echo "Ok so, you're working on" ${bold}${red}$os ${normal}"and you're using" ${bold}${red}$shell
+read -p ${normal}"Is that correct? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "${green}ok i'm gonna install jekyll now"
+  else
+    clear
+    jekyll.sh
+fi
+
 
 # installRuby
